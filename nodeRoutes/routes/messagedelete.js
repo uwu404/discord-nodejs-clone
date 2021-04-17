b9ea85cf2d2c4356f2b7d84ffc5bdc9465b8fcff
Huntah13 <82615765+uwu404@users.noreply.github.com>
2021-04-17 10:34:03 +0900

    fixed another HUGE mistake

--- nodeRoutes/routes/messagedelete.js
+++ nodeRoutes/routes/messagedelete.js
@@ -3,10 +3,10 @@ const Message = require("../../models/message")
 const Channel = require("../../models/channel")
 
 function deleteMessage(app, io) {
-    app.patch("/channels/:channel/messages/:message", async (req, res) => {
+    app.delete("/channels/:channel/messages/:message", async (req, res) => {
         const user = await User.findOne({ token: req.headers.authorization })
         const channel = await Channel.findOne({ _id: req.params.channel })
-        if (!user || channel) return res.status = 404
+        if (!user || !channel) return res.status = 404
         const message = await Message.findByIdAndDelete(req.params.message)
         res.send(message)
         io.to(channel._id).emit("messageDelete", message._id)
