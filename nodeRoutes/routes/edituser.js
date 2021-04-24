965122317a545592e4351eb4086ea1db578cb016
Huntah13 <82615765+uwu404@users.noreply.github.com>
2021-04-24 15:06:45 +0900

    Update edituser.js

--- nodeRoutes/routes/edituser.js
+++ nodeRoutes/routes/edituser.js
@@ -10,17 +10,21 @@ function editavatar(app) {
             const grabage = await Image.findOne({ name: `${user._id}.webp` })
                 .catch(err => console.log(err))
             if (req.body.imageType !== 'gif') {
-                const icon = sharp(Buffer.from(req.body.data.split(",")[1], "base64"))
-                icon.webp({ quality: 50, loop: 0 })
-                icon.resize(200, 200)
-                const data = await icon.toBuffer()
-                save(data)
+                if (req.body.imageType === "resizedgifimage") {
+                    save(req.body.data.split(",")[1])
+                } else {
+                    const icon = sharp(Buffer.from(req.body.data.split(",")[1], "base64"))
+                    icon.webp({ quality: 50, loop: 0 })
+                    icon.resize(200, 200)
+                    const data = await icon.toBuffer()
+                    save(data)
+                }
             } else {
                 const width = parseInt(req.body.dimensions.width)
                 const height = parseInt(req.body.dimensions.height)
                 const toSize = (height < width ? height : width) - 1
-                const x = height > width ? 0 : Math.floor((width - height)/2)
-                const y = height < width ? 0 : Math.floor((height - width)/2)
+                const x = height > width ? 0 : Math.floor((width - height) / 2)
+                const y = height < width ? 0 : Math.floor((height - width) / 2)
                 const resizeGif = await gifsicle(Buffer.from(req.body.data.split(",")[1], "base64"), `${x},${y}+${toSize}x${toSize}`).catch(() => console.log("error resizing gif"))
                 save(resizeGif)
             }
