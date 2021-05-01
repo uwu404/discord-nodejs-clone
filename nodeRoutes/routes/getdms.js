112bf7bff077e6bfa8ed7f2b65c666c9a22c8eb4
Huntah13 <82615765+uwu404@users.noreply.github.com>
2021-05-01 13:40:42 +0900

    Update getdms.js

--- nodeRoutes/routes/getdms.js
+++ nodeRoutes/routes/getdms.js
@@ -8,8 +8,9 @@ function getDirectMessages(app) {
         const reciever = await User.findById(req.params.user)
         if (!user || !reciever) return res.status(500).send("oops something went wrong")
         const dm = await Dms.findOne({ users: { $all: [`${user._id}`, `${reciever._id}`] } })
-    
-        res.send(dm.messages)
+        const list = dm.messages
+        const messages = await Message.find({ _id: { $in: list } })
+        res.send(messages)
     })
 }
 
