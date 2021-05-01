6bc2106fd9bf0950a611501b9fe0a87825fd3559
Huntah13 <82615765+uwu404@users.noreply.github.com>
2021-05-01 14:03:52 +0900

    Update getdms.js

--- nodeRoutes/routes/getdms.js
+++ nodeRoutes/routes/getdms.js
@@ -1,5 +1,5 @@
 const User = require("../../models/user")
-const Message = require("../../models/channel")
+const Message = require("../../models/message")
 const Dms = require("../../models/dms")
 
 function getDirectMessages(app) {
@@ -8,9 +8,18 @@ function getDirectMessages(app) {
         const reciever = await User.findById(req.params.user)
         if (!user || !reciever) return res.status(500).send("oops something went wrong")
         const dm = await Dms.findOne({ users: { $all: [`${user._id}`, `${reciever._id}`] } })
-        const list = dm.messages
-        const messages = await Message.find({ _id: { $in: list } })
-        res.send(messages)
+        const messages = await Message.find({ _id: { $in: dm.messages } })
+        const result = messages.map(m => {
+            const author = m.author === `${user._id}` ? user : reciever
+            const message = {
+                _id: m._id,
+                content: m.content,
+                timestamp: m.timestamp,
+                author: { username: author.username, _id: author._id, avatarURL: author.avatarURL, tag: author.tag }
+            }
+            return message
+        })
+        res.send(result)
     })
 }
 
