649448caad34f7c19a94155beb605dc9d5165897
Huntah13 <82615765+uwu404@users.noreply.github.com>
2021-05-05 23:31:30 +0900

    Update getdmlogs.js

--- nodeRoutes/routes/getdmlogs.js
+++ nodeRoutes/routes/getdmlogs.js
@@ -6,8 +6,7 @@ function getDmslogs(app) {
         const user = await User.findOne({ token: req.headers.authorization })
         if (!user) return res.status(500).send("error")
         const dms = await Dm.find({ users: user._id })
-        const usersId = Array.from(dms.users, d => d.find(u => u !== user._id))
-        res.send(usersId)
+        res.send(dms)
     })
 }
 
