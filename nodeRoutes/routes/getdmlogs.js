26177aa929f81430a283fb225a95021df5ffcb44
Huntah13 <82615765+uwu404@users.noreply.github.com>
2021-05-05 23:26:46 +0900

    Update getdmlogs.js

--- nodeRoutes/routes/getdmlogs.js
+++ nodeRoutes/routes/getdmlogs.js
@@ -4,18 +4,10 @@ const Dm = require("../../models/dms")
 function getDmslogs(app) {
     app.get("/dms/logs", async (req, res) => {
         const user = await User.findOne({ token: req.headers.authorization })
+        if (!user) return res.status(500).send("error")
         const dms = await Dm.find({ users: user._id })
         const usersId = Array.from(dms.users, d => d.find(u => u !== user._id))
-        const users = await User.find({ _id: { $in: usersId } })
-        const logs = Array.from(users, u => {
-            return {
-                username: u.username,
-                avatarURL: u.avatarURL,
-                _id: u._id,
-                tag: u.tag
-            }
-        })
-        res.send(logs)
+        res.send(usersId)
     })
 }
 
