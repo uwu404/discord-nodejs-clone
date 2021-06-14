ca8d0665abf10cb1339d11574d0b1413f8c08caf
bowser <82615765+uwu404@users.noreply.github.com>
2021-06-14 20:15:23 +0900

    Update getuser.js

--- nodeRoutes/routes/getuser.js
+++ nodeRoutes/routes/getuser.js
@@ -1,12 +1,20 @@
 const User = require("../../models/user")
+const Server = require("../../models/server")
+const Channel = require("../../models/channel")
 
 function getuser(app) {
-    app.get("/user/password=:password&email=:email", (req, res) => {
-        User.findOne({ password: req.params.password, email: req.params.email })
-            .then(result => {
-                res.send(result)
-            })
-            .catch(console.log)
+    app.get("/user/password=:password&email=:email", async (req, res) => {
+        const user = await User.findOne({ password: req.params.password, email: req.params.email }).catch(console.log)
+        if (!user) res.status(500).send("oops something went wrong.")
+        const servers = await Server.find({ members: user._id }).catch(console.log)
+        const ids = Array.from(servers, s => s._id)
+        const channels = await Channel.find({ server: { $in: ids } })
+        const serversWithChannels = Array.from(servers, s => {
+            const serverChannels = channels.filter(c => c.server === s._id)
+            return Object.assign(s, { channels: serverChannels })
+        })
+        const USER = Object.assign(user, { servers: serversWithChannels })
+        res.send(USER)
     })
 }
 
