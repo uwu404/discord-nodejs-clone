4fd0a02a01b6841d1b1984b9017d0046ae7521cb
bowser <82615765+uwu404@users.noreply.github.com>
2022-01-08 07:17:03 +0100

    fixed some errors

--- gifTools/index.js
+++ gifTools/index.js
@@ -1,12 +1,13 @@
-const execa = require("execa")
+const execa = import("execa")
 
 const convertGif = async (buffer, { height, width, format, quality }) => {
+    const { default: exec } = await execa
     const base64 = buffer.toString("base64")
     const args = ["gifTools/main.py"]
     if (height || width) args.push("-r", `${width || height}x${height || width}`)
     if (format) args.push("-f", format)
     if (quality) args.push("-q", quality)
-    const { stdout } = await execa("python", args, {
+    const { stdout } = await exec("python", args, {
         encoding: "utf-8",
         maxBuffer: Infinity,
         input: base64
